1a21a49d1fd71d42f9576753d177c88078121eb9
luxisimon <luxisimon@gmail.com>
2019-10-24 21:14:10 -0300

    Update FileSim.cs

--- Codigo/FileSim.cs
+++ Codigo/FileSim.cs
@@ -2,10 +2,22 @@ using System;
 using System.Text;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 
 namespace FireSim
 {
+        public struct Indicadores
+    {
+        private float tSatisfaccion;
+        private float tLectura;
+        private float tEscritura;
+        private float tGestionTotal;
+        private float tMax;
+        private float tMin;
+    }
+
+
     public class FileSim
     {
         private int tprocesamiento;
@@ -18,7 +30,7 @@ namespace FireSim
         private int ContadorOp;
         private Dictionary<string, Indicadores> indicadorArchivo; //para cada 
                                                                   //string NombreArchivo se tiene asociado una estructura Indicadores que almacena los resultados de la simulacion
-        
+
         public FileSim()
         {
             // En el constructor de FileSim se crearia el array de operaciones (vacio)
@@ -27,13 +39,18 @@ namespace FireSim
 
         //En la pantalla de config el simulador obtendra todos los siguientes parametros
         //para crear el disp
-        public void cargaConfig(int tProc, string orgFisica, string algBusqueda, string admEspacio, int metAcceso, 
+        public void cargaConfig(int tProc, string orgFisica, string algBusqueda, string admEspacio, int metAcceso,
                                 int tLectura, int tEscritura, int tSeek, int tamBloques, int tamDispositivo)
         {
 
-            //setters de los otros parametros
+            //setters parametros fileSim
+            SetTprocesamiento(tProc);
+            SetOrganizacionFisica(orgFisica);
+            SetAlgoritmoBusqueda(algBusqueda);
+            SetAdminEspacio(admEspacio);
+            SetMetocoAcceso(metAcceso);
 
-            //Se crea el dispositivo 
+            //Se crea el dispositivo con sus respectivos parametros
             this.disp = new Dispositivo(tLectura, tEscritura, tSeek, tamBloques, tamDispositivo);
 
         }
@@ -41,7 +58,37 @@ namespace FireSim
         public void CargarOperaciones(string ruta)
         {
             // Lee el archivo, se cargan las operaciones en this.TablaOperaciones y se ordena por tArribo
-            // Se genera el mapa
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(ruta))
+                {
+                    string linea;
+                    string [] separa;
+
+                    while ((linea = sr.ReadLine()) != null) //leo hasta que alcanzo el final del archivo
+                    {
+                        separa = linea.Split(';');
+
+                        Operacion op = new Operacion(separa[0],  char.Parse(separa[1]), int.Parse(separa[2]), int.Parse(separa[3]), int.Parse(separa[4]), int.Parse(separa[5]), 0);
+
+                        TablaOperaciones.Add(op);
+                        
+                        //mapa time stamp para organizar por tiempo de arribo
+
+                        
+                        indicadorArchivo.Add(separa[0],????); //Aca se generaria el mapa??? no tenems ningun indicador todavia ?
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                // Muestro al usuario el error
+                Console.WriteLine("El archivo no pudo ser leido:");
+                Console.WriteLine(e.Message);
+            }
         }
 
         public int CantBloques()
@@ -66,12 +113,18 @@ namespace FireSim
 
         public Operacion GetOperacion() //devuelve la operacion actual?
         {
-            return (Operacion) this.TablaOperaciones[this.ContadorOp];
+            return (Operacion)this.TablaOperaciones[this.ContadorOp];
         }
 
-        
+
         public void SimularSiguienteOp()
         {
+            switch(TablaOperaciones.)
+            {
+                case 'c':
+                    Create();
+
+            }
             //case --> CREATE(n) / WRITE(w) / READ(r) / OPEN(o) / CLOSE(c) / DELETE(d)
         }
 
@@ -158,5 +211,6 @@ namespace FireSim
         }
 
     }
+
 }
 
