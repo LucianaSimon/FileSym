a7b61578ef9bba98ea3598d3699e6b8aac2cb6cd
Federico Cisneros <feede.mdp@gmail.com>
2019-10-24 22:54:20 -0300

    Cambiada Clase Operacion por Struct Done cargarArchivo

--- Codigo/FileSim.cs
+++ Codigo/FileSim.cs
@@ -7,17 +7,6 @@ using System.IO;
 
 namespace FireSim
 {
-        public struct Indicadores
-    {
-        private float tSatisfaccion;
-        private float tLectura;
-        private float tEscritura;
-        private float tGestionTotal;
-        private float tMax;
-        private float tMin;
-    }
-
-
     public class FileSim
     {
         private int tprocesamiento;
@@ -30,16 +19,17 @@ namespace FireSim
         private int ContadorOp;
         private Dictionary<string, Indicadores> indicadorArchivo; //para cada 
                                                                   //string NombreArchivo se tiene asociado una estructura Indicadores que almacena los resultados de la simulacion
-
+        
         public FileSim()
         {
             // En el constructor de FileSim se crearia el array de operaciones (vacio)
             this.TablaOperaciones = new ArrayList();
+            this.SetContadorOp(0);
         }
 
         //En la pantalla de config el simulador obtendra todos los siguientes parametros
         //para crear el disp
-        public void cargaConfig(int tProc, string orgFisica, string algBusqueda, string admEspacio, int metAcceso,
+        public void cargaConfig(int tProc, string orgFisica, string algBusqueda, string admEspacio, int metAcceso, 
                                 int tLectura, int tEscritura, int tSeek, int tamBloques, int tamDispositivo)
         {
 
@@ -50,7 +40,7 @@ namespace FireSim
             SetAdminEspacio(admEspacio);
             SetMetocoAcceso(metAcceso);
 
-            //Se crea el dispositivo con sus respectivos parametros
+            //Se crea el dispositivo 
             this.disp = new Dispositivo(tLectura, tEscritura, tSeek, tamBloques, tamDispositivo);
 
         }
@@ -58,37 +48,42 @@ namespace FireSim
         public void CargarOperaciones(string ruta)
         {
             // Lee el archivo, se cargan las operaciones en this.TablaOperaciones y se ordena por tArribo
+            // Se genera el mapa
+            
             try
             {
-                // Create an instance of StreamReader to read from a file.
-                // The using statement also closes the StreamReader.
-                using (StreamReader sr = new StreamReader(ruta))
+                // El metodo ReadAllLines de File, cierra el archivo automaticamente.
+                string[] lineas = File.ReadAllLines(ruta);
+            
+                Operacion opAux = new Operacion();
+                foreach (var linea in lineas)
                 {
-                    string linea;
-                    string [] separa;
-
-                    while ((linea = sr.ReadLine()) != null) //leo hasta que alcanzo el final del archivo
-                    {
-                        separa = linea.Split(';');
-
-                        Operacion op = new Operacion(separa[0],  char.Parse(separa[1]), int.Parse(separa[2]), int.Parse(separa[3]), int.Parse(separa[4]), int.Parse(separa[5]), 0);
-
-                        TablaOperaciones.Add(op);
-                        
-                        //mapa time stamp para organizar por tiempo de arribo
-
-                        
-                        indicadorArchivo.Add(separa[0],????); //Aca se generaria el mapa??? no tenems ningun indicador todavia ?
-                    }
-
+                    var valores = linea.Split(";");
+                    opAux.NombreArchivo = valores[0];
+                    opAux.IdOperacion = char.Parse(valores[1]);
+                    opAux.NumProceso = Int32.Parse(valores[2]);
+                    opAux.Tarribo = Int32.Parse(valores[3]);
+                    opAux.Offset = Int32.Parse(valores[4]);
+                    opAux.Estado = 0;
+                    TablaOperaciones.Add(opAux);
                 }
+
+                TablaOperaciones.Sort(new ComparaOp());
+                
+                /*  Bloque solo de testeo de metodo Sort*/
+                foreach(Operacion op in TablaOperaciones)
+                {
+                    Console.WriteLine(op.NombreArchivo + " " + op.IdOperacion + " " + op.NumProceso + " " +
+                        op.Tarribo + " " + op.Offset + " " + op.Estado);
+                }
+                
+
             }
             catch (Exception e)
             {
-                // Muestro al usuario el error
-                Console.WriteLine("El archivo no pudo ser leido:");
-                Console.WriteLine(e.Message);
+                Console.WriteLine("Error: " + e);
             }
+
         }
 
         public int CantBloques()
@@ -96,36 +91,56 @@ namespace FireSim
             return this.disp.GetCantBloques();
         }
 
-        public int EstadoBloque() //que hace este metodo?
+        // EstadoBloque devuelve un float de 0 a 1 indicando el estado del bloque
+        // si es 0 el bloque esta vacio, si es 1 esta lleno, lo demas es el porcentaje de ocupacion del bloque
+        public float EstadoBloque(int bloque) //que hace este metodo?
         {
-            throw new Exception("The method or operation is not implemented.");
+            return disp.estadoBloque(bloque);
         }
-
-        public int GetCantidadOp()
-        {
-            return this.TablaOperaciones.Count;
-        }
-
-        public int GetContadorOp()
-        {
-            return this.ContadorOp;
-        }
-
-        public Operacion GetOperacion() //devuelve la operacion actual?
-        {
-            return (Operacion)this.TablaOperaciones[this.ContadorOp];
-        }
-
 
         public void SimularSiguienteOp()
         {
-            switch(TablaOperaciones.)
-            {
-                case 'c':
-                    Create();
-
-            }
             //case --> CREATE(n) / WRITE(w) / READ(r) / OPEN(o) / CLOSE(c) / DELETE(d)
+            switch(Char.ToUpper(((Operacion)TablaOperaciones[GetContadorOp()]).IdOperacion))
+            {
+                case 'N':
+                    {
+                        Create();
+                        break;
+                    }
+                case 'D':
+                    {
+                        Delete();
+                        break;
+                    }
+                case 'O':
+                    {
+                        Open();
+                        break;
+                    }
+                case 'C':
+                    {
+                        Close();
+                        break;
+                    }
+                case 'R':
+                    {
+                        Read();
+                        break;
+                    }
+                case 'W':
+                    {
+                        Write();
+                        break;
+                    }
+
+                default:
+                    {
+                        Console.WriteLine("Operacion Incorrecta, se descarta");
+                        break;
+                    }
+            }
+            SetContadorOp(GetContadorOp() + 1);
         }
 
         public void Create()
@@ -158,8 +173,33 @@ namespace FireSim
             throw new Exception("The method or operation is not implemented.");
         }
 
+        /**
+         * Getters y Setters
+        **/
+
+        public int GetCantidadOp()
+        {
+            return this.TablaOperaciones.Count;
+        }
+
+        public int GetContadorOp()
+        {
+            return this.ContadorOp;
+        }
+
+        public void SetContadorOp(int value)
+        {
+            ContadorOp = value;
+        }
+
+        public Operacion GetOperacion() //devuelve la operacion actual?
+        {
+            return (Operacion)this.TablaOperaciones[this.ContadorOp];
+        }
+
         //Una vez cargada la configuracion vamos a permitir cambiarla, porq esto significa que
         //vamos a tener que modificar al dispositivo tambien (no solo en el constructor de FileSim)!!!
+        // No entendi (Fede)
         public int GetTprocesamiento()
         {
             return tprocesamiento;
@@ -209,8 +249,21 @@ namespace FireSim
         {
             metocoAcceso = value;
         }
-
     }
-
 }
 
+
+public class ComparaOp : IComparer
+{
+    public int Compare(object x, object y)
+    {
+        if (((Operacion)y).Tarribo > ((Operacion)x).Tarribo)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}
--- Codigo/Operacion.cs
+++ Codigo/Operacion.cs
@@ -10,27 +10,14 @@ public enum estadoOp
     Finalizado = 2
 }
 
-public class Operacion
-{ 
-    private string NombreArchivo;
-    private char IdOperacion;
-    private int NumProceso;
-    private int Tarribo;
-    private int Offset;
-    private int CantidadUA;
-    private estadoOp Estado;    //Espera, Ejecutando, Finalizado
-    
-
-    public Operacion(string nombreArchivo, char idOperacion, int numProceso, int tArribo, int offset, int cantidadUA, estadoOp estado)
-    {
-        this.NombreArchivo = nombreArchivo;
-        this.IdOperacion = idOperacion;
-        this.NumProceso = numProceso;
-        this.Tarribo = tArribo;
-        this.Offset = offset;
-        this.CantidadUA = cantidadUA;
-        this.Estado = estado;
-    }
-
+public struct Operacion
+{
+    public string NombreArchivo;
+    public char IdOperacion;
+    public int NumProceso;
+    public int Tarribo;
+    public int Offset;
+    public int CantidadUA;
+    public estadoOp Estado;
 }
 
